8f369fb37619018fbf85320b850f6d75f6e6c4de
蒋龙豪 <3082352063@qq.com>
2017-12-16 17:44:17 +0800

    nothing

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,15 +15,16 @@ echo Html::cssFile('@web/frontend/web/statics/css/index.css');
 </div>
 <div class="post-list">
 	<div>
+        <?php foreach($articals as $artical){ ?>
 		<article class="post post-with-tags">
 			<header class="post-title">
-				<a href="">智能分析最佳实践——指标逻辑树</a>
+				<a href=""><?php echo $artical['title']; ?></a>
 			</header>
 			<div class="post-meta">
-				<span class="post-meta-author">蒋龙豪</span>
-				<span class="post-meta-ctime">2017-12-04</span>
+				<span class="post-meta-author"><?php echo $artical['author']['username']; ?></span>
+				<span class="post-meta-ctime"><?php echo date('Y-m-d', $artical['created_at']) ?></span>
 			</div>
-			<p class="post-abstract">所有业务都会面对“为什么涨、为什么降、原因是什么？”这种简单粗暴又不易定位的业务问题。为了找出数据发生异动的原因，业务人员会通过使用多维查询、dashboard等数据产品锁定问题，再辅助人工分析查找问题原因，这个过程通常需要一天时间。</p>
+			<p class="post-abstract"><?php echo $artical['summary']; ?></p>
 			<footer class="post-tags">
 				<a class="tag">
 					<span class="tag_name">BI</span>
@@ -42,60 +43,7 @@ echo Html::cssFile('@web/frontend/web/statics/css/index.css');
 				</a>
 			</footer>
 		</article>
-		<article class="post post-with-tags">
-			<header class="post-title">
-				<a href="">智能分析最佳实践——指标逻辑树</a>
-			</header>
-			<div class="post-meta">
-				<span class="post-meta-author">蒋龙豪</span>
-				<span class="post-meta-ctime">2017-12-04</span>
-			</div>
-			<p class="post-abstract">所有业务都会面对“为什么涨、为什么降、原因是什么？”这种简单粗暴又不易定位的业务问题。为了找出数据发生异动的原因，业务人员会通过使用多维查询、dashboard等数据产品锁定问题，再辅助人工分析查找问题原因，这个过程通常需要一天时间。</p>
-			<footer class="post-tags">
-				<a class="tag">
-					<span class="tag_name">BI</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">指标</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">逻辑树</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">酒旅</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">系统</span>
-				</a>
-			</footer>
-		</article>
-		<article class="post post-with-tags">
-			<header class="post-title">
-				<a href="">智能分析最佳实践——指标逻辑树</a>
-			</header>
-			<div class="post-meta">
-				<span class="post-meta-author">蒋龙豪</span>
-				<span class="post-meta-ctime">2017-12-04</span>
-			</div>
-			<p class="post-abstract">所有业务都会面对“为什么涨、为什么降、原因是什么？”这种简单粗暴又不易定位的业务问题。为了找出数据发生异动的原因，业务人员会通过使用多维查询、dashboard等数据产品锁定问题，再辅助人工分析查找问题原因，这个过程通常需要一天时间。</p>
-			<footer class="post-tags">
-				<a class="tag">
-					<span class="tag_name">BI</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">指标</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">逻辑树</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">酒旅</span>
-				</a>
-				<a class="tag">
-					<span class="tag_name">系统</span>
-				</a>
-			</footer>
-		</article>
+		<?php } ?>
 	</div>
 </div>
 
