9696d3acb274824a15a551a67a76f70abfa100b5
jianglonghao <1482197005@qq.com>
2017-12-22 17:50:18 +0800

    nothing

--- frontend/models/AccesslogForm.php
+++ frontend/models/AccesslogForm.php
@@ -8,9 +8,30 @@ class AccesslogForm
 {
 	public static function insertLog()
 	{
+		$userIp = Yii::$app->request->getUserIP();
+
+		$ipInfo = self::getIpInfo($userIp);
+
 		$log = new Accesslog;
-		$log->ip = Yii::$app->request->getUserIP();;
+		$log->ip = $userIp;
+		$log->origin = $ipInfo;
 		$log->created_at = time();
 		$log->save();
+	}
+	public static function getIpInfo($ip)
+	{
+		if(empty($ip)){
+			return;
+		}
+		$url = 'http://ip.taobao.com/service/getIpInfo.php?ip=' . $ip;
+
+		$ch = curl_init();
+		curl_setopt($ch, , CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$jsondata = curl_exec($ch);
+		curl_close($ch);
+		$data = json_decode($jsondata);
+		return $data;
 	}
 }
