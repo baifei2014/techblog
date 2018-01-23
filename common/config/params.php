d22c5db6c1251b666448e4065e837dc0b2abd88b
jianglonghao <1482197005@qq.com>
2018-01-23 16:50:07 +0800

    config about params config

--- common/config/params.php
+++ common/config/params.php
@@ -1,11 +1,14 @@
 <?php
-return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'user.passwordResetTokenExpire' => 3600,
-    'devicedetect' => [
-    	'isMobile' => false,
-		'isTablet' => false,
-		'isDesktop' => true,
+return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/params-local.php'),
+    [
+	    'adminEmail' => 'admin@example.com',
+	    'supportEmail' => 'support@example.com',
+	    'user.passwordResetTokenExpire' => 3600,
+	    'devicedetect' => [
+	    	'isMobile' => false,
+			'isTablet' => false,
+			'isDesktop' => true,
+		],
     ],
-];
+);
